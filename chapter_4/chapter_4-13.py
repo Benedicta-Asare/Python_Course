f84ebe51a3d640ee69b9fb7503ae6cd2a02b05e6
Benedicta-Asare <lilbengilla@gmail.com>
2023-06-05 20:10:43 +0000

    create chapter_4-13

---
+++ chapter_4/chapter_4-13.py
@@ -0,0 +1,12 @@
+#Tuple
+foods = ("fried rice", "jollof rice", "noodles", "french fries", "pizza")
+
+print("\nMenu:")
+for food in foods:
+    print(food)
+
+foods = ("fried rice", "jollof rice", "banku", "yam", "pizza")
+
+print("\nRevised menu:")
+for food in foods:
+    print(food)
