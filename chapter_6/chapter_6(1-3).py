58723cc5c69cdd53f07ef01b9c2c9513103e5530
Benedicta-Asare <lilbengilla@gmail.com>
2023-06-10 16:09:39 +0000

    create chapter_6(1-3)

---
+++ chapter_6/chapter_6(1-3).py
@@ -0,0 +1,35 @@
+#6-1. Person
+person = {'first_name': 'Tessa', 'last_name': 'Lynn', 'age': 21, 'city': 'Los Angeles'}
+print(person['first_name'])
+print(person['last_name'])
+print(person['age'])
+print(person['city'])
+
+#6-2. Favorite Numbers
+favorite_numbers = {
+    'Sandy': 11,
+    'Prince': 7,
+    'Nessa': 3,
+    'Stacy': 1,
+    'Ransford': 7,
+    }
+print(f"\n\nSandy: {favorite_numbers['Sandy']}")
+print(f"Prince: {favorite_numbers['Prince']}")
+print(f"Nessa: {favorite_numbers['Nessa']}")
+print(f"Stacy: {favorite_numbers['Stacy']}")
+print(f"Ransford: {favorite_numbers['Ransford']}")
+
+
+#6-3. Glossary
+glossary = {
+    'variable': 'A label that is assigned to a value.',
+    'float': 'A number with a decimal point.',
+    'string': 'A series of characters.',
+    'list': 'A collection of items in a particular order.',
+    'dictionary': 'A collection of key-value pairs.'
+}
+print(f"\n\nVariable: {glossary['variable']}")
+print(f"\nFloat: {glossary['float']}")
+print(f"\nString: {glossary['string']}")
+print(f"\nList: {glossary['list']}")
+print(f"\nDictionary: {glossary['dictionary']}")
