8a14d72867584feada57da63305439a9b892f9dc
Benedicta-Asare <lilbengilla@gmail.com>
2023-11-16 23:59:32 +0000

    Create chapter_7(1-3).py

---
+++ chapter_7/chapter_7(1-3).py
@@ -0,0 +1,30 @@
+#7-1. Rental Car
+
+message = input("What kind of rental car would you like? ")
+
+print(f"\nLet me see if I can find you a {message}.")
+
+
+#7-2. Restaurant Seating
+
+number = input("\nHow many people are in your dinner group? ")
+
+number = int(number)
+
+if number > 8:
+    print("\nYou'll have to wait for a table.")
+else:
+    print("Your table is ready.")
+
+
+#7-3. Multiples of Ten
+
+multiple = input("\nIf you give me a number, I'll tell you if it's a multiple of 10 or not.")
+multiple += "\nEnter a number: "
+
+multiple = int(multiple)
+
+if multiple % 10 == 0:
+    print(f"\n{multiple} is a multiple of 10.")
+else:
+    print(f"\n{multiple} is not a multiple of 10.")
