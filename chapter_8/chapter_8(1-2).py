72f30151ecbcd51faf14092addd96a951cf3f9c7
Benedicta-Asare <lilbengilla@gmail.com>
2023-11-17 16:21:11 +0000

    Create chapter_8(1-2).py

---
+++ chapter_8/chapter_8(1-2).py
@@ -0,0 +1,14 @@
+#8-1. Message
+
+def display_message():
+    print("\nI'm learning about functions in this chapter.")
+
+display_message()
+
+
+#8-2. Favorite Book
+
+def favorite_book(title):
+    print(f"\nOne of my favorite books is {title}.")
+
+favorite_book('Alice in Wonderland')
