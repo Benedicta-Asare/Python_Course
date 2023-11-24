8f54a1482965e6b1528eadeb210bdc32061851b3
Benedicta-Asare <lilbengilla@gmail.com>
2023-11-24 22:02:42 +0000

    Create user.py

---
+++ chapter_9/chapter_9-12/user.py
@@ -0,0 +1,16 @@
+class User:
+    def __init__(self, first_name, last_name, age, email, location):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+        self.email = email
+        self.location = location
+
+    def describe_user(self):
+        print(f"\nFull name: {self.first_name} {self.last_name}")
+        print(f"Age: {self.age}")
+        print(f"Email: {self.email}")
+        print(f"Location: {self.location}")
+
+    def greet_user(self):
+        print(f"\nHello {self.first_name}, welcome!")
