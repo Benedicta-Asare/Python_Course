3d58a0de3ca0ee7c057c3700cc23c88208a7065c
Benedicta-Asare <lilbengilla@gmail.com>
2023-11-24 22:03:15 +0000

    Create admin.py

---
+++ chapter_9/chapter_9-12/admin.py
@@ -0,0 +1,6 @@
+from adminclass import Admin
+
+admin = Admin('Tara', 'Thompson', '27', 'tara@example.com', 'Brooklyn')
+admin.describe_user()
+admin.greet_user()
+admin.get_privileges.show_privileges()
