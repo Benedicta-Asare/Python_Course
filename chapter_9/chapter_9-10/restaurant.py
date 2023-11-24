d0021c187512bf4fb5d14c23fb17a7f92e9451df
Benedicta-Asare <lilbengilla@gmail.com>
2023-11-24 17:21:24 +0000

    Create restaurant.py

---
+++ chapter_9/chapter_9-10/restaurant.py
@@ -0,0 +1,19 @@
+class Restaurant:
+    def __init__(self, restaurant_name, cuisine_type):
+        self.restaurant_name = restaurant_name
+        self.cuisine_type = cuisine_type
+
+    def describe_restaurant(self):
+        print(f"Restaurant name: {self.restaurant_name}")
+        print(f"Cuisine type: {self.cuisine_type}")
+
+
+class IceCreamStand(Restaurant):
+    def __init__(self, restaurant_name, flavors):
+        super().__init__(restaurant_name, cuisine_type='Ice Cream Shop')
+        self.flavors = flavors
+
+    def display_flavors(self):
+        print("Ice cream flavors available:")
+        for flavor in self.flavors:
+            print(flavor)
